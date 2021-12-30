4a07f6e97911cb52d5b9a3d46043112c858797cf
Habin Hong <binscot0317@gmail.com>
2021-12-31 02:42:19 +0900

    feat(detail):comment

--- app.py
+++ app.py
@@ -14,11 +14,23 @@ def home():
     return render_template('index.html')
 
 
+# dtail 화면 보여주기
+@app.route('/detail.html')
+def detail():
+    return render_template('detail.html')
+
+
+# popup 화면 보여주기
+@app.route('/popup.html')
+def popup():
+    return render_template('popup.html')
+
+
 # API 역할을 하는 부분
 @app.route('/api/list', methods=['GET'])
 def show_music():
-    music = list(db.mymusic.find({}, {'_id': False}).sort('like',-1))
-    return jsonify({'musics': music })
+    music = list(db.mymusic.find({}, {'_id': False}).sort('like', -1))
+    return jsonify({'musics': music})
 
 
 @app.route('/api/like', methods=['POST'])
@@ -33,6 +45,27 @@ def like_star():
     db.mymusic.update_one({'name': name_receive}, {'$set': {'like': new_like}})
 
     return jsonify({'msg': '좋아요 완료!'})
+
+
+# 댓글 저장
+@app.route('/comment', methods=['POST'])
+def make_comment():
+    nick_receive = request.form['nick_give']
+    comment_receive = request.form['comment_give']
+
+    doc = {'nick': nick_receive,
+           'comment': comment_receive,
+           }
+    db.toy_comment.insert_one(doc)
+
+    return jsonify({'msg': '댓글 등록 완료!'})
+
+
+# 댓글 보기
+@app.route('/comment', methods=['GET'])
+def read_comment():
+    comments = list(db.toy_comment.find({}, {'_id': False}))
+    return jsonify({'all_comments': comments})
 
 
 if __name__ == '__main__':
