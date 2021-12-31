c0f346f343df59f7a492fb23576466776bdf6491
aaanjini <zini6633@gmail.com>
2021-12-31 17:44:20 +0900

    상세화면 추가

--- app.py
+++ app.py
@@ -18,6 +18,11 @@ db = client.dbsparta
 def home():
     return render_template('index.html')
 
+# dtail 화면 보여주기
+@app.route('/detail.html')
+def detail():
+    return render_template('detail.html')
+
 
 # API 역할을 하는 부분
 @app.route('/api/list', methods=['GET'])
@@ -38,6 +43,26 @@ def like_star():
     db.mymusic.update_one({'name': name_receive}, {'$set': {'like': new_like}})
 
     return jsonify({'msg': '좋아요 완료!'})
+
+# 댓글 저장
+@app.route('/comment', methods=['POST'])
+def make_comment():
+    nick_receive = request.form['nick_give']
+    comment_receive = request.form['comment_give']
+
+    doc = {'nick': nick_receive,
+           'comment': comment_receive,
+           }
+    db.toy_comment.insert_one(doc)
+
+    return jsonify({'msg': '댓글 등록 완료!'})
+
+
+# 댓글 보기
+@app.route('/comment', methods=['GET'])
+def read_comment():
+    comments = list(db.toy_comment.find({}, {'_id': False}))
+    return jsonify({'all_comments': comments})
 
 
  
@@ -83,7 +108,7 @@ def added_music(id):
     music_added = db.mymusic.find_one({'_id':ObjectId(id)})
 
     artist = music_added['artist']
-    url =  music_added['imgUrl']
+    url = music_added['imgUrl']
     name = music_added['name']
  
     return jsonify({"artist":artist, 'url':url, 'name':name ,'msg':'보여주기'})
